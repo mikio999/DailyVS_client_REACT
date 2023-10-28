68e23e9bbf43330d264aa45bdd8dfe5610d964a0
minkyung_kwak <ellen10min@gmail.com>
2023-10-29 02:15:45 +0900

    Add: feature/41

--- src/pages/Result/ResultGraph/ResultGraph.js
+++ src/pages/Result/ResultGraph/ResultGraph.js
@@ -9,88 +9,146 @@ import AgeGraph from './AgeGraph/AgeGraph.js';
 
 const ResultGraph = ({ voteResult }) => {
   console.log('voteResult', voteResult);
-  const man_choices = [];
-  const woman_choices = [];
-  const e_choices = [];
-  const i_choices = [];
-  const s_choices = [];
-  const n_choices = [];
-  const t_choices = [];
-  const f_choices = [];
-  const p_choices = [];
-  const j_choices = [];
 
-  for (let i = 1; i <= voteResult.statistics?.choice_count; i++) {
-    const manPercentageKey = `choice${i}_M_percentage`;
-    const womanPercentageKey = `choice${i}_W_percentage`;
-    const EPercentageKey = `choice${i}_E_percentage`;
-    const IPercentageKey = `choice${i}_I_percentage`;
-    const SPercentageKey = `choice${i}_S_percentage`;
-    const NPercentageKey = `choice${i}_N_percentage`;
-    const TPercentageKey = `choice${i}_T_percentage`;
-    const FPercentageKey = `choice${i}_F_percentage`;
-    const PPercentageKey = `choice${i}_P_percentage`;
-    const JPercentageKey = `choice${i}_J_percentage`;
+  const man_choices = voteResult.statistics?.gender.M;
+  const woman_choices = voteResult.statistics?.gender.W;
 
-    man_choices.push(voteResult.statistics[manPercentageKey]);
-    woman_choices.push(voteResult.statistics[womanPercentageKey]);
-    e_choices.push(voteResult.statistics[EPercentageKey]);
-    i_choices.push(voteResult.statistics[IPercentageKey]);
-    s_choices.push(voteResult.statistics[SPercentageKey]);
-    n_choices.push(voteResult.statistics[NPercentageKey]);
-    t_choices.push(voteResult.statistics[TPercentageKey]);
-    f_choices.push(voteResult.statistics[FPercentageKey]);
-    p_choices.push(voteResult.statistics[PPercentageKey]);
-    j_choices.push(voteResult.statistics[JPercentageKey]);
+  const e_choices = voteResult.statistics?.mbti.E;
+  const i_choices = voteResult.statistics?.mbti.I;
+  const s_choices = voteResult.statistics?.mbti.S;
+  const n_choices = voteResult.statistics?.mbti.N;
+  const t_choices = voteResult.statistics?.mbti.T;
+  const f_choices = voteResult.statistics?.mbti.F;
+  const p_choices = voteResult.statistics?.mbti.P;
+  const j_choices = voteResult.statistics?.mbti.J;
+
+  const choices_10 = voteResult.statistics?.age['10'];
+  const choices_20_1 = voteResult.statistics?.age['20_1'];
+  const choices_20_2 = voteResult.statistics?.age['20_2'];
+  const choices_30_1 = voteResult.statistics?.age['30_1'];
+  const choices_30_2 = voteResult.statistics?.age['30_2'];
+  const choices_40 = voteResult.statistics?.age['40'];
+
+  const man_choicesArray = [];
+  const woman_choicesArray = [];
+
+  const e_choicesArray = [];
+  const i_choicesArray = [];
+  const s_choicesArray = [];
+  const n_choicesArray = [];
+  const t_choicesArray = [];
+  const f_choicesArray = [];
+  const p_choicesArray = [];
+  const j_choicesArray = [];
+
+  const choicesArray_10 = [];
+  const choicesArray_20_1 = [];
+  const choicesArray_20_2 = [];
+  const choicesArray_30_1 = [];
+  const choicesArray_30_2 = [];
+  const choicesArray_40 = [];
+
+  for (let i = 1; i <= 5; i++) {
+    const choiceKey = `choice${i}`;
+
+    if (man_choices?.hasOwnProperty(choiceKey)) {
+      man_choicesArray.push(man_choices[choiceKey]);
+    }
+    if (woman_choices?.hasOwnProperty(choiceKey)) {
+      woman_choicesArray.push(woman_choices[choiceKey]);
+    }
+    if (e_choices?.hasOwnProperty(choiceKey)) {
+      e_choicesArray.push(e_choices[choiceKey]);
+    }
+    if (i_choices?.hasOwnProperty(choiceKey)) {
+      i_choicesArray.push(i_choices[choiceKey]);
+    }
+    if (s_choices?.hasOwnProperty(choiceKey)) {
+      s_choicesArray.push(s_choices[choiceKey]);
+    }
+    if (n_choices?.hasOwnProperty(choiceKey)) {
+      n_choicesArray.push(n_choices[choiceKey]);
+    }
+    if (t_choices?.hasOwnProperty(choiceKey)) {
+      t_choicesArray.push(t_choices[choiceKey]);
+    }
+    if (f_choices?.hasOwnProperty(choiceKey)) {
+      f_choicesArray.push(f_choices[choiceKey]);
+    }
+    if (p_choices?.hasOwnProperty(choiceKey)) {
+      p_choicesArray.push(p_choices[choiceKey]);
+    }
+    if (j_choices?.hasOwnProperty(choiceKey)) {
+      j_choicesArray.push(j_choices[choiceKey]);
+    }
+    if (choices_10?.hasOwnProperty(choiceKey)) {
+      choicesArray_10.push(choices_10[choiceKey]);
+    }
+    if (choices_20_1?.hasOwnProperty(choiceKey)) {
+      choicesArray_20_1.push(choices_20_1[choiceKey]);
+    }
+    if (choices_20_2?.hasOwnProperty(choiceKey)) {
+      choicesArray_20_2.push(choices_20_2[choiceKey]);
+    }
+    if (choices_30_1?.hasOwnProperty(choiceKey)) {
+      choicesArray_30_1.push(choices_30_1[choiceKey]);
+    }
+    if (choices_30_2?.hasOwnProperty(choiceKey)) {
+      choicesArray_30_2.push(choices_30_2[choiceKey]);
+    }
+    if (choices_40?.hasOwnProperty(choiceKey)) {
+      choicesArray_40.push(choices_40[choiceKey]);
+    }
   }
 
-  console.log('choices: ', voteResult.poll?.choices);
-  console.log('t_choices:', t_choices);
-  console.log('f_choices:', f_choices);
   return (
     <GraphContainer>
       <GraphCategory>카테고리</GraphCategory>
-      <GenderGraph
-        choices={voteResult.poll?.choices}
-        man_choices={man_choices}
-        woman_choices={woman_choices}
-      />
-      <EIGraph
-        choices={voteResult.poll?.choices}
-        e_choices={e_choices}
-        i_choices={i_choices}
-      />
-      <SNGraph
-        choices={voteResult.poll?.choices}
-        s_choices={s_choices}
-        n_choices={n_choices}
-      />
-      <TFGraph
-        choices={voteResult.poll?.choices}
-        t_choices={t_choices}
-        f_choices={f_choices}
-      />
-      <PJGraph
-        choices={voteResult.poll?.choices}
-        p_choices={p_choices}
-        j_choices={j_choices}
-      />
-      <AgeGraph
-        option1={voteResult.option_1}
-        option2={voteResult.option_2}
-        choice1_10_Percentage={voteResult.choice1_10_percentage}
-        choice2_10_Percentage={voteResult.choice2_10_percentage}
-        choice1_20_1_Percentage={voteResult.choice1_20_1_percentage}
-        choice2_20_1_Percentage={voteResult.choice2_20_1_percentage}
-        choice1_20_2_Percentage={voteResult.choice1_20_2_percentage}
-        choice2_20_2_Percentage={voteResult.choice2_20_2_percentage}
-        choice1_30_1_Percentage={voteResult.choice1_30_1_percentage}
-        choice2_30_1_Percentage={voteResult.choice2_30_1_percentage}
-        choice1_30_2_Percentage={voteResult.choice1_30_2_percentage}
-        choice2_30_2_Percentage={voteResult.choice2_30_2_percentage}
-        choice1_40_Percentage={voteResult.choice1_40_percentage}
-        choice2_40_Percentage={voteResult.choice2_40_percentage}
-      />
+      {voteResult.poll?.category.some(item => item.name === 'gender') && (
+        <GenderGraph
+          choices={voteResult.poll?.choices}
+          man_choices={man_choicesArray}
+          woman_choices={woman_choicesArray}
+        />
+      )}
+      {voteResult.poll?.category.some(item => item.name === 'mbti') && (
+        <>
+          <EIGraph
+            choices={voteResult.poll?.choices}
+            e_choices={e_choicesArray}
+            i_choices={i_choicesArray}
+          />
+
+          <SNGraph
+            choices={voteResult.poll?.choices}
+            s_choices={s_choicesArray}
+            n_choices={n_choicesArray}
+          />
+
+          <TFGraph
+            choices={voteResult.poll?.choices}
+            t_choices={t_choicesArray}
+            f_choices={f_choicesArray}
+          />
+
+          <PJGraph
+            choices={voteResult.poll?.choices}
+            p_choices={p_choicesArray}
+            j_choices={j_choicesArray}
+          />
+        </>
+      )}
+      {voteResult.poll?.category.some(item => item.name === 'age') && (
+        <AgeGraph
+          choices={voteResult.poll?.choices}
+          choices_10={choicesArray_10}
+          choices_20_1={choicesArray_20_1}
+          choices_20_2={choicesArray_20_2}
+          choices_30_1={choicesArray_30_1}
+          choices_30_2={choicesArray_30_2}
+          choices_40={choicesArray_40}
+        />
+      )}
     </GraphContainer>
   );
 };
