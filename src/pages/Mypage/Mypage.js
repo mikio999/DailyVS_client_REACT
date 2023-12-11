2af819b8c497d2f814ef8ba95fa6e2f5e816f806
minkyung_kwak <ellen10min@gmail.com>
2023-12-11 18:56:27 +0900

    #296 mypage logout btn delete

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -38,20 +38,6 @@ const Mypage = () => {
       });
   }, []);
 
-  const logout_user = () => {
-    const shouldLogout = window.confirm('로그아웃 하시겠습니까?');
-    if (shouldLogout) {
-      localStorage.removeItem('isKakao');
-      if (selectedKakaoAuth) {
-        dispatch(kakao_logout());
-        setRedirect(true);
-      } else {
-        logout();
-        setRedirect(true);
-      }
-    }
-  };
-
   if (loading) return;
   return (
     <Container>
@@ -61,12 +47,6 @@ const Mypage = () => {
       <MypageVoteList />
       <MypageCreateList />
       <MypageLikeList />
-
-      <LogoutContainer>
-        <LogOut onClick={logout_user} style={{ cursor: 'pointer' }}>
-          로그아웃
-        </LogOut>
-      </LogoutContainer>
     </Container>
   );
 };
@@ -84,28 +64,4 @@ const InformationTitle = styled.h1`
   font-family: 'GongGothicMedium';
   font-size: 28px;
   margin: 20px auto;
-`;
-
-const LogoutContainer = styled.div`
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 50px 0;
-  @media screen and (min-width: 600px) {
-    display: none;
-  }
-`;
-
-const LogOut = styled.div`
-  background-color: ${theme.colors.redpinkPrimaryColor};
-  color: white;
-  padding: 10px 15px;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: white;
-    border: 1px solid ${theme.colors.redpinkPrimaryColor};
-    color: ${theme.colors.redpinkPrimaryColor};
-  }
 `;
